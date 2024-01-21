7a807e534b68e2489ed392b932f9fdf29f50e535
daikennki <dai45402@gmail.com>
2024-01-21 19:49:28 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,12 +3,13 @@ class CommentsController < ApplicationController
     def create
       @prototype = Prototype.find(params[:prototype_id])
       puts "Prototype found: #{@prototype.inspect}" # デバッグプリントを追加
-      @comment = @prototype.comments.build(comment_params.merge(user_id: current_user.id, prototype_id: @prototype.id))
+      @comment = @prototype.comments.build(comment_params.merge(user_id: current_user.id))
       
 
       if @comment.save
         redirect_to prototype_path(@prototype)
       else
+        puts "Comment not saved. Errors: #{@comment.errors.full_messages}" # エラー発生時にエラーメッセージを出力するデバッグプリントを追加
         render "prototypes/show"
       end
     end
