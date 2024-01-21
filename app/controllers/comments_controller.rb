a99cf743da2022926d236aee12a306604986e7f9
daikennki <dai45402@gmail.com>
2024-01-22 08:38:00 +0900

    変更４

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,25 @@
 class CommentsController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+  def create
+    @prototype = Prototype.find_by(id: params[:prototype_id])
 
-    def create
-      @prototype = Prototype.find_by(id: params[:prototype_id])
-      puts "Prototype found: #{@prototype.inspect}" # デバッグプリントを追加
+    if @prototype
       @comment = @prototype.comments.build(comment_params.merge(user_id: current_user.id))
-      
 
       if @comment.save
         redirect_to prototype_path(@prototype)
       else
-        puts "Comment not saved. Errors: #{@comment.errors.full_messages}" # エラー発生時にエラーメッセージを出力するデバッグプリントを追加
+        puts "Comment not saved. Errors: #{@comment.errors.full_messages}"
         render "prototypes/show"
       end
-    end
-
-    private
-
-    def comment_params
-        params.require(:comment).permit(:content)
-    end
-
-    def handle_record_not_found
+    else
       puts "Prototype not found! Params: #{params.inspect}"
-      # 他に必要な処理があればここで追加
-      render status: 404, plain: 'Prototype not found!'
+      render "prototypes/show"
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
