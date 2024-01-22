0f816071af2f6701d8bb7113def4e0d6bc62485d
daikennki <dai45402@gmail.com>
2024-01-22 10:16:34 +0900

    修正7

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,23 @@
+# app/controllers/comments_controller.rb
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
 
   def create
-    @prototype = Prototype.find_by(id: params[:prototype_id])
-    @comment = @prototype.comments.build(comment_params.merge(user_id: current_user.id))
+    @prototype = Prototype.find(params[:prototype_id])
+    @comment = @prototype.comments.build(comment_params)
 
     if @comment.save
-      redirect_to prototype_path(@prototype)
+      redirect_to prototype_path(@prototype), notice: 'コメントが投稿されました。'
     else
-      render "prototypes/show"
+      # コメントの保存に失敗した場合、再度showアクションを表示
+      @comments = @prototype.comments.includes(:user)
+      render 'prototypes/show'
     end
   end
 
   private
 
   def comment_params
-      params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
