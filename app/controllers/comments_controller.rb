aa8186e03e0862ca3370fdd087da7f2938f25e53
daikennki <dai45402@gmail.com>
2024-01-22 09:05:50 +0900

    変更6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
   def create
-    @prototype = Prototype.find_by(params[:prototype_id])
+    @prototype = Prototype.find_by(id: params[:prototype_id])
     @comment = @prototype.comments.build(comment_params.merge(user_id: current_user.id))
 
     if @comment.save
